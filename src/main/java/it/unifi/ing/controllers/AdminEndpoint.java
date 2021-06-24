24c6c9fee6b54253b9c15a32d8fe6d138596cbe6
Francesco <francesco.areoluci@gmail.com>
2021-06-24 11:45:28 +0200

    Added product edit endpoint. Code refactoring

--- src/main/java/it/unifi/ing/controllers/AdminEndpoint.java
+++ src/main/java/it/unifi/ing/controllers/AdminEndpoint.java
@@ -183,24 +183,7 @@ public class AdminEndpoint {
         List<Locale> localeList = localeDao.getLocaleList();
 
         // Check for valid product locales
-        boolean invalidLanguage = false;
-        boolean localeFound = false;
-        for (LocalizedProductDto lpDto : localizedProductDtoList) {
-            for (Locale l : localeList) {
-                if (l.getCountryCode().equals(lpDto.getCountry()) &&
-                    l.getLanguageCode().equals(lpDto.getLocale())) {
-                    localeFound = true;
-                    break;
-                }
-            }
-
-            if (!localeFound) {
-                invalidLanguage = true;
-                break;
-            }
-        }
-
-        if (invalidLanguage) {
+        if (checkForInvalidLocaleInDto(localeList, localizedProductDtoList)) {
             logger.error("Sent product contains an invalid locale");
             return Response.status(404).build();
         }
@@ -208,28 +191,12 @@ public class AdminEndpoint {
         // Retrieve available currencies
         List<Currency> currencyList = currencyDao.getCurrencyList();
 
-        // Check for valid product currencies
-        boolean invalidCurrency = false;
-        boolean currencyFound = false;
-        for (LocalizedProductDto lpDto : localizedProductDtoList) {
-            for (Currency c : currencyList) {
-                if (c.getCurrency().equals(lpDto.getCurrency())) {
-                    currencyFound = true;
-                    break;
-                }
-            }
-
-            if (!currencyFound) {
-                invalidCurrency = true;
-                break;
-            }
-        }
-
-        if (invalidCurrency) {
+        if (checkForInvalidCurrencyInDto(currencyList, localizedProductDtoList)) {
             logger.error("Sent product contains an invalid currency");
             return Response.status(404).build();
         }
 
+        // Check for manufacturers in dto
         Manufacturer manufacturer = manufacturerDao.getManufacturerByName(productDto.getManufacturer());
         if (manufacturer == null) {
             // Create new one
@@ -243,41 +210,147 @@ public class AdminEndpoint {
         }
 
         // Create product
-        Product product = ModelFactory.product();
-        product.setProdManufacturer(manufacturer);
-        product.setProdAdministrator(admin);
-
-        List<LocalizedProduct> localizedProductList = new ArrayList<>();
-        for (LocalizedProductDto lpDto : localizedProductDtoList) {
-            LocalizedProduct lp = ModelFactory.localizedProduct();
-            lp.setProduct(product);
-            for (Locale l : localeList) {
-                if (lpDto.getLocale().equals(l.getLanguageCode()) &&
-                    lpDto.getCountry().equals(l.getCountryCode())) {
-                    lp.setLocale(l);
-                    break;
-                }
-            }
-            lp.setName(lpDto.getName());
-            lp.setDescription(lpDto.getDescription());
-            lp.setCategory(lpDto.getCategory());
-            for (Currency c : currencyList) {
-                if (lpDto.getCurrency().equals(c.getCurrency())) {
-                    lp.setCurrency(c);
-                }
-            }
-            lp.setPrice(lpDto.getPrice());
-
-            localizedProductList.add(lp);
-        }
-
-        product.setLocalizedProductList(localizedProductList);
+        Product product = buildProduct(admin, manufacturer, localeList, currencyList, productDto);
 
         productDao.addEntity(product);
 
         logger.info("Persisted a new product with id: " + product.getId());
 
         return Response.status(200).build();
+    }
+
+    /**
+     * @implNote Edit a product and/or its localization info. Currently
+     *           does not support the adding of a localization, only
+     *           editing of the existing ones.
+     * @param headers
+     * @param productDto
+     * @return
+     */
+    @PUT
+    @Path("/products/edit")
+    @JWTTokenNeeded(Permissions = UserRole.ADMIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response editProduct(@Context HttpHeaders headers,
+                                ProductDto productDto)
+    {
+        logger.debug("Requested /products/edit endpoint");
+
+        // Get username from token
+        String token = JWTUtil.extractBearerHeader(headers);
+        if (token.isEmpty()) {
+            logger.error("Empty bearer header");
+            return Response.status(401).build();
+        }
+        String username = JWTUtil.getUsernameFromToken(token);
+        if (username == null) {
+            logger.error("Cannot get username from token");
+            return Response.status(401).build();
+        }
+
+        // Check for valid product
+        Product product = productDao.getEntityById(productDto.getId());
+        if (product == null) {
+            logger.error("Unable to retrieve product with id: " + productDto.getId());
+            return Response.status(404).build();
+        }
+
+        // Check for valid product localization
+        List<LocalizedProduct> localizedProductList = product.getLocalizedProductList();
+        if (localizedProductList.isEmpty()) {
+            logger.error("Empty localization list for product with id: " + productDto.getId());
+            return Response.status(404).build();
+        }
+
+        // Get admin entity
+        Admin admin = adminDao.getAdminByUsername(username);
+        if (admin == null) {
+            logger.error("Unable to retrieve user " + username);
+            return Response.status(404).build();
+        }
+
+        // Check for product localizations in dto
+        List<LocalizedProductDto> localizedProductDtoList = productDto.getLocalizedInfo();
+        if (localizedProductDtoList.isEmpty()) {
+            logger.error("Sent product does not contain any localization info");
+            return Response.status(404).build();
+        }
+
+        // Retrieve available locales
+        List<Locale> localeList = localeDao.getLocaleList();
+
+        // Check for valid product locales
+        if (checkForInvalidLocaleInDto(localeList, localizedProductDtoList)) {
+            logger.error("Sent product contains an invalid locale");
+            return Response.status(404).build();
+        }
+
+        // Retrieve available currencies
+        List<Currency> currencyList = currencyDao.getCurrencyList();
+
+        if (checkForInvalidCurrencyInDto(currencyList, localizedProductDtoList)) {
+            logger.error("Sent product contains an invalid currency");
+            return Response.status(404).build();
+        }
+
+        // Check that the sent localizations have the same id of the persisted one
+        if (checkForInvalidLocalizationInDto(localizedProductList, localizedProductDtoList)) {
+            logger.error("Sent product contains an invalid localization identifier");
+            return Response.status(404).build();
+        }
+
+        // Check for manufacturer in dto
+        Manufacturer manufacturer = manufacturerDao.getManufacturerByName(productDto.getManufacturer());
+        if (manufacturer == null) {
+            // Create new one
+            logger.info("The following non-existing manufacturer will be created: " +
+                    productDto.getManufacturer());
+            manufacturer = ModelFactory.manufacturer();
+            manufacturer.setName(productDto.getManufacturer());
+
+            manufacturerDao.addEntity(manufacturer);
+            logger.info("Persisted a new manufacturer with id: " + manufacturer.getId());
+        }
+
+        // Edit product
+        product.setProdManufacturer(manufacturer);
+        product.setProdAdministrator(admin);
+
+        for (LocalizedProductDto lpDto : localizedProductDtoList) {
+            for (LocalizedProduct lp : localizedProductList) {
+                if (lp.getId().equals(lpDto.getId())) {
+                    lp.setProduct(product);
+                    for (Locale l : localeList) {
+                        if (lpDto.getLocale().equals(l.getLanguageCode()) &&
+                                lpDto.getCountry().equals(l.getCountryCode())) {
+                            lp.setLocale(l);
+                            break;
+                        }
+                    }
+                    lp.setName(lpDto.getName());
+                    lp.setDescription(lpDto.getDescription());
+                    lp.setCategory(lpDto.getCategory());
+                    for (Currency c : currencyList) {
+                        if (lpDto.getCurrency().equals(c.getCurrency())) {
+                            lp.setCurrency(c);
+                        }
+                    }
+                    lp.setPrice(lpDto.getPrice());
+
+                    break;
+                }
+            }
+        }
+
+        product.setLocalizedProductList(localizedProductList);
+
+        productDao.updateEntity(product);
+
+        logger.info("Edited product with id: " + product.getId());
+
+        return Response.status(200).build();
+
     }
 
     @DELETE
@@ -297,7 +370,7 @@ public class AdminEndpoint {
         // Remove cart products references
         List<CartProduct> cartProductList = cartProductDao.getCartProductList();
         for (CartProduct pc : cartProductList) {
-            if (pc.getProduct().getId() == productId) {
+            if (pc.getProduct().getId().equals(productId)) {
                 cartProductDao.deleteEntity(pc);
             }
         }
@@ -305,7 +378,7 @@ public class AdminEndpoint {
         // Remove purchased products references
         List<PurchasedProduct> purchasedProductList = purchasedProductDao.getPurchasedProductList();
         for (PurchasedProduct pp : purchasedProductList) {
-            if (pp.getProduct().getId() == productId) {
+            if (pp.getProduct().getId().equals(productId)) {
                 purchasedProductDao.deleteEntity(pp);
             }
         }
@@ -436,6 +509,114 @@ public class AdminEndpoint {
         logger.info("Persisted a new currency with id: " + currency.getId());
 
         return Response.status(200).build();
+    }
+
+    private boolean checkForInvalidLocaleInDto(List<Locale> localeList,
+                                             List<LocalizedProductDto> localizedProductDtoList)
+    {
+        boolean invalidLanguage = false;
+        boolean localeFound = false;
+        for (LocalizedProductDto lpDto : localizedProductDtoList) {
+            for (Locale l : localeList) {
+                if (l.getCountryCode().equals(lpDto.getCountry()) &&
+                        l.getLanguageCode().equals(lpDto.getLocale())) {
+                    localeFound = true;
+                    break;
+                }
+            }
+
+            if (!localeFound) {
+                invalidLanguage = true;
+                break;
+            }
+        }
+
+        return invalidLanguage;
+    }
+
+    private boolean checkForInvalidCurrencyInDto(List<Currency> currencyList,
+                                               List<LocalizedProductDto> localizedProductDtoList)
+    {
+        boolean invalidCurrency = false;
+        boolean currencyFound = false;
+        for (LocalizedProductDto lpDto : localizedProductDtoList) {
+            for (Currency c : currencyList) {
+                if (c.getCurrency().equals(lpDto.getCurrency())) {
+                    currencyFound = true;
+                    break;
+                }
+            }
+
+            if (!currencyFound) {
+                invalidCurrency = true;
+                break;
+            }
+        }
+
+        return invalidCurrency;
+    }
+
+    private boolean checkForInvalidLocalizationInDto(List<LocalizedProduct> localizedProductList,
+                                                    List<LocalizedProductDto> localizedProductDtoList)
+    {
+        boolean invalidLocalization = false;
+        boolean localizationFound = false;
+        for (LocalizedProductDto lpDto : localizedProductDtoList) {
+            for (LocalizedProduct lc : localizedProductList) {
+                if (lc.getId().equals(lpDto.getId())) {
+                    localizationFound = true;
+                    break;
+                }
+            }
+
+            if (!localizationFound) {
+                invalidLocalization = true;
+                break;
+            }
+        }
+
+        return invalidLocalization;
+    }
+
+    private Product buildProduct(Admin admin,
+                              Manufacturer manufacturer,
+                              List<Locale> localeList,
+                              List<Currency> currencyList,
+                              ProductDto productDto)
+    {
+        List<LocalizedProductDto> localizedProductDtoList = productDto.getLocalizedInfo();
+
+        Product product = ModelFactory.product();
+        product.setProdManufacturer(manufacturer);
+        product.setProdAdministrator(admin);
+
+        List<LocalizedProduct> localizedProductList = new ArrayList<>();
+        for (LocalizedProductDto lpDto : localizedProductDtoList) {
+            LocalizedProduct lp = ModelFactory.localizedProduct();
+            lp.setProduct(product);
+            for (Locale l : localeList) {
+                if (lpDto.getLocale().equals(l.getLanguageCode()) &&
+                        lpDto.getCountry().equals(l.getCountryCode())) {
+                    lp.setLocale(l);
+                    break;
+                }
+            }
+            lp.setName(lpDto.getName());
+            lp.setDescription(lpDto.getDescription());
+            lp.setCategory(lpDto.getCategory());
+            for (Currency c : currencyList) {
+                if (lpDto.getCurrency().equals(c.getCurrency())) {
+                    lp.setCurrency(c);
+                }
+            }
+            lp.setPrice(lpDto.getPrice());
+
+            localizedProductList.add(lp);
+        }
+
+        product.setLocalizedProductList(localizedProductList);
+
+        return product;
     }
 
 }
